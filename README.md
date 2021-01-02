28b53a9d2cfa5d179fa40ca5d1c6c1848447c090
zhangyang <2506465056@qq.com>
2021-01-02 15:57:44 +0800

    vue组件初步项目搭建完成

--- README.md
+++ README.md
@@ -32,3 +32,27 @@ yarn run test:unit
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 开发过程
+
+参考 https://juejin.cn/post/6844903808787546125
+
+ * src目录改为example目录 用来测试组件
+ * 新建packages目录 用来写组件
+ * 新建vue.config.js 配置路径
+
+ * packages/test 编写测试组件
+ * 在main.js 引入组件包，测试组件
+ * 打包 配置package.json
+ ```
+    // target: 默认为构建应用，改为 lib 即可启用构建库模式
+    // name: 输出文件名
+    // dest: 输出目录，默认为 dist，这里我们改为 lib
+    // entry: 入口文件路径
+    vue-cli-service build --target lib --name lib [entry]
+
+    "lib": "vue-cli-service build --target lib --name zyjs-ui --dest lib packages/index.js"
+
+ ```
+  * npm run lib 生成包目录
+  
---
+++ packages/test/src/test.vue
@@ -0,0 +1,36 @@
+<!--test.vue-->
+<template>
+  <div class="xr-test" @click="handleClick">{{ num }}</div>
+</template>
+
+<script>
+export default {
+  name: "XrTest", // 这个名字很重要，它就是未来的标签名<xr-test></xr-test>，坑了我一下
+  data() {
+    return {
+      num: 0
+    };
+  },
+  methods: {
+    handleClick() {
+      this.num++;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.xr-test {
+  width: 100px;
+  height: 100px;
+  line-height: 100px;
+  border-radius: 50%;
+  font-size: 30px;
+  text-align: center;
+  background: #24292e;
+  color: white;
+}
+</style>
+
+作者：尤水就下 链接：https://juejin.cn/post/6844903808787546125 来源：掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
